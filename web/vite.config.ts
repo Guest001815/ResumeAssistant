f9e47724360cc4be9e6ddee4767ae2fed6d8bc7f
liujinwen <2811455492@qq.com>
2026-01-13 13:56:31 +0800

    2026-1-13-fix: 生产环境配置修复

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     port: 5178
   },
   resolve: {
     alias: {
-      scheduler: "scheduler/cjs/scheduler.development.js"
+      scheduler: mode === 'production' 
+        ? "scheduler/cjs/scheduler.production.min.js"
+        : "scheduler/cjs/scheduler.development.js"
     }
   }
-});
+}));
